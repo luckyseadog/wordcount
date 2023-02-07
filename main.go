b67d96be98b6674d873ea8f44e810797d0e2a51c
логин с cpp0.manytask.or <vasiliyasp@gmail.com>
2023-02-07 14:18:28 +0300

    commit2

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+import "strings"
+
+//func readString() string {
+//	rdr := bufio.NewReader(os.Stdin)
+//	str, _ := rdr.ReadString('\n')
+//	return str
+//}
+
+func main() {
+	s := os.Args[1]
+
+	fmt.Print(len(strings.Fields(s)))
+}
